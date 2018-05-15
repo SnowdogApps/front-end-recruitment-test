adb3d7d799efc6a4809ec0e7940dc21b4a2cf6f9
Bartek Igielski <igloczek@gmail.com>
2018-05-15 17:06:27 +0200

    Updated dependecies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -119,7 +119,7 @@ gulp.task('scripts', () =>
       .pipe($.sourcemaps.write())
       .pipe(gulp.dest('.tmp/scripts'))
       .pipe($.concat('main.min.js'))
-      .pipe($.uglify({preserveComments: 'some'}))
+      .pipe($.uglify())
       // Output files
       .pipe($.size({title: 'scripts'}))
       .pipe($.sourcemaps.write('.'))
