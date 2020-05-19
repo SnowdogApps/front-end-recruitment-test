846d6a3fd3433e09bf1460566f3bc760d6ceaf61
Tomek Różewicz <tomek.rozewicz@snow.dog>
2020-05-19 15:16:06 +0200

    #67077 - Add parcel.js && remove gulp

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,7 +38,7 @@
     'serviceWorker' in navigator
     && (window.location.protocol === 'https:' || isLocalhost)
   ) {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('../service-worker.js')
       .then(function(registration) {
         // updatefound is fired if service-worker.js changes.
         registration.onupdatefound = function() {
---
+++ plugins/submodule.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.symlinkSync('../submodule/submodule.html', 'app/submodule.html');
