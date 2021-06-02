eeb857281a99ae88d44fbf6306085e5b373ddefd
Bartek Igielski <bartek.igielski@snow.dog>
2021-06-02 16:46:32 +0200

    Order API endpoint

--- start/routes.ts
+++ start/routes.ts
@@ -19,21 +19,53 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-
-const state = {
-  cart: {
-    items: [
-      { name: 'Apple Watch Sport', price: 580 },
-      { name: 'Modern Buckle', price: 380 },
-    ],
-    totals: {
-      suoTotal: 960,
-      tax: 0,
-      grandToal: 960,
-    },
-  },
-}
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 Route.get('/', async ({ view }) => {
+  const state = {
+    cart: {
+      items: [
+        { name: 'Apple Watch Sport', price: 580 },
+        { name: 'Modern Buckle', price: 380 },
+      ],
+      totals: {
+        suoTotal: 960,
+        tax: 0,
+        grandToal: 960,
+      },
+    },
+  }
+
   return view.render('index', state)
+})
+
+Route.post('/order', async ({ request, response }) => {
+  const orderSchema = schema.create({
+    firstName: schema.string(),
+    lastName: schema.string(),
+    email: schema.string({}, [
+      rules.email(),
+    ]),
+    phone: schema.string({}, [
+      rules.mobile(),
+    ]),
+    creditCard: schema.string({}, [
+      rules.regex(new RegExp('^[0-9]{16}$')),
+    ]),
+    CVV: schema.string({}, [
+      rules.regex(new RegExp('^[0-9]{3}$')),
+    ]),
+    expDate: schema.string({}, [
+      rules.regex(new RegExp('^[0-9]{2}\/[0-9]{2}$')),
+    ]),
+  })
+
+  try {
+    const body = await request.validate({ schema: orderSchema })
+    response.send({
+      message: 'Order successfully placed.',
+    })
+  } catch(error) {
+    response.badRequest(error.messages)
+  }
 })
